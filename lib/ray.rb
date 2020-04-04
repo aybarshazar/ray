be6a62e4d2cdb2514d4068f38448cdcef5b8e95b
Aybars Hazar Şimşir <aybarshazar@gmail.com>
2020-04-04 17:37:03 +0300

    Return 404 for favicon requests
    Ideally this will be handled by the framework or the web server. We'll
    just handle it to prevent our logs from being polluted by errors.

--- lib/ray.rb
+++ lib/ray.rb
@@ -5,6 +5,10 @@ require "ray/array"
 module Ray
   class Application
     def call(env)
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, { "Content-Type" => "text/html" }, []]
+      end
+
       klass, action = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(action)
