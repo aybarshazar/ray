766de1d101cea97ba1f3952c12a7f269e701dc7b
Aybars Hazar Şimşir <aybarshazar@gmail.com>
2020-04-26 13:36:04 +0300

    Use instance variables for views
    Instead of passing a hash for local variables during render, we'll just
    set instance variables that will be available in views automatically
    (similar to Rails).
    
    Before:
    
    render :home, { :message => "Hello" }
    
    After:
    
    @message = "Hello"
    render :home

--- lib/ray/controller.rb
+++ lib/ray/controller.rb
@@ -10,17 +10,25 @@ module Ray
       @env
     end
 
-    def render(view_name, locals ={})
+    def render(view_name)
       filename = File.join("app", "views", controller_name, "#{view_name}.html.erb")
       template = File.read(filename)
       eruby = Erubis::Eruby.new(template)
-      eruby.result(locals.merge(:env => env))
+      eruby.result(view_variables.merge(:env => env))
     end
 
     def controller_name
       klass = self.class
       klass = klass.to_s.gsub(/Controller$/, "")
       Ray.to_underscore(klass)
+    end
+
+    def view_variables
+      vars = {}
+      (instance_variables - [:@env]).each do |variable|
+        vars[variable] = instance_variable_get(variable)
+      end
+      vars
     end
   end
 end
