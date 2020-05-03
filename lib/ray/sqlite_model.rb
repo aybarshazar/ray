49fcb7ad20ec5645c03d749e7c927bb3de3510e1
Aybars Hazar Şimşir <aybarshazar@gmail.com>
2020-05-03 20:38:25 +0300

    Add	create method
    We convert the values to their respective SQLite data types before
    inserting.
    
    last_insert_rowid() is used to get the id of the last inserted record.
    
    https://www.sqlite.org/c3ref/last_insert_rowid.html
    
    After insertion, we return an object that represents the row.
    
    The method could be used in an app as follows:
    
    record = MyTable.create("title" => "It happened!", "posted" => 1, "body" => "It did!")
    puts record.inspect

--- lib/ray/sqlite_model.rb
+++ lib/ray/sqlite_model.rb
@@ -6,6 +6,42 @@ DB = SQLite3::Database.new "ray.db"
 module Ray
   module Model
     class SQLite
+      def initialize(data = nil)
+        @hash = data
+      end
+
+      def self.create(values)
+        values.delete("id")
+        keys = schema.keys - ["id"]
+
+        converted_values = keys.map do |key|
+          values[key] ? to_sql(values[key]) : "null"
+        end
+
+        DB.execute <<~SQL
+          INSERT INTO #{table} (#{keys.join(", ")}) VALUES (#{converted_values.join(", ")});
+        SQL
+
+        raw_vals = keys.map { |k| values[k] }
+        data = Hash[keys.zip(raw_vals)]
+        sql = "SELECT last_insert_rowid();"
+        data["id"] = DB.execute(sql)[0][0]
+        self.new(data)
+      end
+
+      def self.to_sql(val)
+        case val
+        when NilClass
+          "null"
+        when Numeric
+          val.to_s
+        when String
+          "'#{val}'"
+        else
+          raise "Can't change #{val.class} to SQL!"
+        end
+      end
+
       def self.table
         Ray.to_underscore(name)
       end
